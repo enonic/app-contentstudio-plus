9acb1b604ead6fa1c74dd7f266ccb0c8bb9b5a15
Mikita Taukachou <edloidas@gmail.com>
2020-12-01 14:29:17 +0300

    Integrate lib-contentstudio in the build #5
    Added example to test the lib-contentstudio in Widget.
    Removed tsconfig.build.json.
    Added node_modules installation for lib-contentstudio.

--- src/main/resources/assets/js/widgets/Widget.ts
+++ src/main/resources/assets/js/widgets/Widget.ts
@@ -1,6 +1,7 @@
 import {DivEl} from 'lib-admin-ui/dom/DivEl';
 import {LoadMask} from 'lib-admin-ui/ui/mask/LoadMask';
 import {AppHelper} from '../util/AppHelper';
+import {OnOffButton} from 'lib-contentstudio/app/issue/view/OnOffButton';
 
 export class Widget
     extends DivEl {
@@ -8,6 +9,7 @@ export class Widget
     private readonly loadMask: LoadMask;
     private readonly contentId: string;
     private readonly repository: string;
+    private readonly button: OnOffButton;
 
     constructor(config: WidgetConfig, cls?: string) {
         super(AppHelper.getCommonWidgetClass() + (' ' + cls || ''));
@@ -15,6 +17,9 @@ export class Widget
         this.loadMask = new LoadMask(this);
         this.contentId = config.contentId;
         this.repository = config.repository;
+
+        this.button = new OnOffButton({off: false, offLabel: 'Turn On', onLabel: 'Turn Off'});
+        this.appendChild(this.button);
     }
 }
 
