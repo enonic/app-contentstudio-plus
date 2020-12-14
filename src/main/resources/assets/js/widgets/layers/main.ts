1ca3cc1bcc1a8f1571ab2b3093c6cb9a7658b190
Aliaksandr Shklianko <asl@enonic.com>
2020-12-14 10:43:44 +0300

    Move Layers widget from Content Studio #4
    -removing 18n for now

--- src/main/resources/assets/js/widgets/layers/main.ts
+++ src/main/resources/assets/js/widgets/layers/main.ts
@@ -2,13 +2,10 @@ import {Element} from 'lib-admin-ui/dom/Element';
 import {WidgetConfig} from '../Widget';
 import {LayersWidget} from '../layers/LayersWidget';
 import {AppHelper} from '../../util/AppHelper';
-import {i18nAdd} from 'lib-admin-ui/util/MessagesInitializer';
 
 declare const CS_PLUS_LAYERS_CONFIG: WidgetConfig;
 
-(async () => {
-    await i18nAdd(CS_PLUS_LAYERS_CONFIG.i18nUrl);
-
+(() => {
     const widgetContainer = document.getElementById(AppHelper.getLayersWidgetClass());
 
     if (widgetContainer) {
