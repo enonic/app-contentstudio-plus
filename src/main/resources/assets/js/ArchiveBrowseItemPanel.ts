ec21698b19b99b718fe569a4ae06a69c8a125110
Aliaksandr Shklianko <asl@enonic.com>
2021-07-23 13:53:09 +0300

    -adding archivation date into preview toolbar for the future use

---
+++ src/main/resources/assets/js/ArchiveBrowseItemPanel.ts
@@ -0,0 +1,11 @@
+import {BrowseItemPanel} from 'lib-admin-ui/app/browse/BrowseItemPanel';
+import { ContentBrowseItemPanel } from 'lib-contentstudio/app/browse/ContentBrowseItemPanel';
+import {ArchiveItemStatisticsPanel} from './ArchiveItemStatisticsPanel';
+
+export class ArchiveBrowseItemPanel
+    extends BrowseItemPanel {
+
+    createItemStatisticsPanel(): ArchiveItemStatisticsPanel {
+        return new ArchiveItemStatisticsPanel();
+    }
+}
