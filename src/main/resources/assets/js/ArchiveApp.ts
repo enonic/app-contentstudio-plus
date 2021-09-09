82140fe6184df1607f29c13d9fa197d997761786
Aliaksandr Shklianko <asl@enonic.com>
2021-09-09 14:07:17 +0300

    Investigate on possibility to open archive app in same window with settings and studio #3470

--- src/main/resources/assets/js/ArchiveApp.ts
+++ src/main/resources/assets/js/ArchiveApp.ts
@@ -2,12 +2,13 @@ import {App} from 'lib-contentstudio/app/App';
 import {AppContainer} from 'lib-contentstudio/app/AppContainer';
 import {i18n} from 'lib-admin-ui/util/Messages';
 import {ArchiveAppContainer} from './ArchiveAppContainer';
+import {DescriptorKey} from 'lib-contentstudio/app/page/DescriptorKey';
 
 export class ArchiveApp
     extends App {
 
     constructor() {
-        super('plus');
+        super(DescriptorKey.fromString(`${CONFIG.appId}.plus:main`));
     }
 
     protected createAppContainer(): AppContainer {
