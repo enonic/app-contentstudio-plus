6975fa9e10b36bcec3eb2c11340cbdf77db89244
Aliaksandr Shklianko <asl@enonic.com>
2021-10-11 16:41:25 +0300

    Fix urls for the admin tools #229

--- src/main/resources/assets/js/ArchiveAppContainer.ts
+++ src/main/resources/assets/js/ArchiveAppContainer.ts
@@ -4,8 +4,19 @@ import {AppBar} from 'lib-admin-ui/app/bar/AppBar';
 import {AppPanel} from 'lib-admin-ui/app/AppPanel';
 import {ArchiveAppBar} from './ArchiveAppBar';
 import {ArchiveAppPanel} from './ArchiveAppPanel';
+import {ProjectContext} from 'lib-contentstudio/app/project/ProjectContext';
+import {ArchiveResourceRequest} from './resource/ArchiveResourceRequest';
 
-export class ArchiveAppContainer extends AppContainer {
+export class ArchiveAppContainer
+    extends AppContainer {
+
+    constructor() {
+        super();
+
+        this.onShown(() => {
+            history.pushState(null, null, `main#/${ProjectContext.get().getProject().getName()}/${ArchiveResourceRequest.ARCHIVE_PATH}`);
+        });
+    }
 
     protected createAppBar(application: Application): AppBar {
         return new ArchiveAppBar(application);
