6cc068d7cce56748f5c8d48129e5ebc835a12619
Aliaksandr Shklianko <asl@enonic.com>
2021-10-12 13:17:37 +0300

    Implement preview for non-binary content in the Archive #223

--- src/main/resources/assets/js/ArchiveItemPreviewToolbar.ts
+++ src/main/resources/assets/js/ArchiveItemPreviewToolbar.ts
@@ -3,17 +3,26 @@ import {DateTimeFormatter} from 'lib-admin-ui/ui/treegrid/DateTimeFormatter';
 import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
 import {ContentStatusToolbar} from 'lib-contentstudio/app/ContentStatusToolbar';
 import {i18n} from 'lib-admin-ui/util/Messages';
+import {DivEl} from 'lib-admin-ui/dom/DivEl';
 
 export class ArchiveItemPreviewToolbar
     extends ContentStatusToolbar {
 
-    private whenArchived: SpanEl;
+    private readonly whenArchived: SpanEl;
+
+    private readonly originalPathEl: SpanEl;
 
     constructor() {
         super('archive-status-toolbar');
 
         this.whenArchived = new SpanEl('when');
         this.whenArchived.insertAfterEl(this.status);
+
+        const originalPathWrapper = new DivEl('original-path');
+        this.originalPathEl = new SpanEl('part2');
+        originalPathWrapper.appendChildren(new SpanEl('part1').setHtml(i18n('preview.path.part1')), this.originalPathEl);
+
+        this.insertChild(originalPathWrapper, 1);
     }
 
     setItem(item: ContentSummaryAndCompareStatus): void {
@@ -22,6 +31,7 @@ export class ArchiveItemPreviewToolbar
         if (item) {
             this.status.setHtml(i18n('status.archived'));
             this.whenArchived.setHtml(DateTimeFormatter.createHtmlNoTimestamp(item.getContentSummary().getModifiedTime()));
+            this.originalPathEl.setHtml(item.getPath().toString());
         }
     }
 
@@ -29,5 +39,9 @@ export class ArchiveItemPreviewToolbar
         super.clearItem();
 
         this.whenArchived.setHtml('');
+    }
+
+    protected foldOrExpand(): void {
+        return;
     }
 }
