6bb78249470f6396346de9caf40bd28cbc35c0e6
Aliaksandr Shklianko <asl@enonic.com>
2021-10-18 16:01:51 +0300

    Add context panel to the Archive #224

--- src/main/resources/assets/js/ArchiveBrowsePanel.ts
+++ src/main/resources/assets/js/ArchiveBrowsePanel.ts
@@ -4,14 +4,53 @@ import {Toolbar} from 'lib-admin-ui/ui/toolbar/Toolbar';
 import {ArchiveTreeGrid} from './ArchiveTreeGrid';
 import {ArchiveBrowseItemPanel} from './ArchiveBrowseItemPanel';
 import * as Q from 'q';
+import {Button} from 'lib-admin-ui/ui/button/Button';
+import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
+import {ToggleContextPanelEvent} from 'lib-contentstudio/app/view/context/ToggleContextPanelEvent';
+import {SplitPanel} from 'lib-admin-ui/ui/panel/SplitPanel';
+import {ContextSplitPanel} from 'lib-contentstudio/app/view/context/ContextSplitPanel';
+import {ContextPanel} from 'lib-contentstudio/app/view/context/ContextPanel';
+import {ActiveContextPanelManager} from 'lib-contentstudio/app/view/context/ActiveContextPanelManager';
+import {DockedContextPanel} from 'lib-contentstudio/app/view/context/DockedContextPanel';
+import {ArchiveContextView} from './ArchiveContextView';
+import {ArchiveContentViewItem} from './ArchiveContentViewItem';
 
 export class ArchiveBrowsePanel
     extends BrowsePanel {
+
+    protected treeGrid: ArchiveTreeGrid;
+
+    private contextView: ArchiveContextView;
 
     protected initElements(): void {
         super.initElements();
 
         this.browseToolbar.addActions(this.getBrowseActions().getAllActions());
+        this.addContextPanelButton();
+    }
+
+    private addContextPanelButton() {
+        const button: Button = new Button();
+        button.addClass('toggle-button icon-list');
+        this.browseToolbar.appendChild(button);
+
+        button.onClicked(() => {
+            new ToggleContextPanelEvent().fire();
+            button.toggleClass('expanded', !button.hasClass('expanded'));
+        });
+    }
+
+    protected updatePreviewItem(): void {
+        super.updatePreviewItem();
+
+        const item: ArchiveContentViewItem = <ArchiveContentViewItem>this.treeGrid.getLastSelectedOrHighlightedItem();
+        const summary: ContentSummaryAndCompareStatus = item?.getData();
+        const contextPanel: ContextPanel = ActiveContextPanelManager.getActiveContextPanel();
+
+        if (contextPanel) {
+            contextPanel.setItem(summary);
+            this.contextView.setArchiveItem(item);
+        }
     }
 
     protected createTreeGrid(): ArchiveTreeGrid {
@@ -26,15 +65,23 @@ export class ArchiveBrowsePanel
         return new ArchiveBrowseItemPanel();
     }
 
-    protected getFirstPanelSize(): number {
-        return 62;
-    }
-
     doRender(): Q.Promise<boolean> {
         return super.doRender().then((rendered: boolean) => {
             this.addClass('archive-browse-panel');
 
             return rendered;
         });
+    }
+
+    protected createBrowseWithItemsPanel(): SplitPanel {
+        this.contextView = new ArchiveContextView();
+
+        const contextSplitPanel: ContextSplitPanel = ContextSplitPanel.create(this.getBrowseItemPanel(),
+            new DockedContextPanel(this.contextView))
+            .setActions(this.getBrowseActions().getAllActions())
+            .setContextView(this.contextView)
+            .build();
+
+        return contextSplitPanel;
     }
 }
---
+++ src/main/resources/assets/js/ArchiveContextView.ts
@@ -0,0 +1,49 @@
+import {ContextView} from 'lib-contentstudio/app/view/context/ContextView';
+import * as Q from 'q';
+import {WidgetView} from 'lib-contentstudio/app/view/context/WidgetView';
+import {WidgetItemView} from 'lib-contentstudio/app/view/context/WidgetItemView';
+import {AttachmentsWidgetItemView} from 'lib-contentstudio/app/view/context/widget/details/AttachmentsWidgetItemView';
+import {ContentWidgetItemView} from 'lib-contentstudio/app/view/context/widget/details/ContentWidgetItemView';
+import {ContentPath} from 'lib-contentstudio/app/content/ContentPath';
+import {ArchiveStatusWidgetItemView} from './widgets/ArchiveStatusWidgetItemView';
+import {ArchiveContentViewItem} from './ArchiveContentViewItem';
+import {ArchivePropertiesWidgetItemView} from './widgets/ArchivePropertiesWidgetItemView';
+
+export class ArchiveContextView
+    extends ContextView {
+
+    private archiveItem: ArchiveContentViewItem;
+
+    private contentWidgetItemView: ContentWidgetItemView;
+
+    private propertiesWidgetItemView: ArchivePropertiesWidgetItemView;
+
+    getCustomWidgetViewsAndUpdateDropdown(): Q.Promise<void> {
+        this.widgetsSelectionRow.updateWidgetsDropdown(this.widgetViews);
+        return Q(null);
+    }
+
+    protected getInitialWidgets(): WidgetView[] {
+        return [this.propertiesWidgetView, this.createVersionsWidgetView()];
+    }
+
+    protected getDetailsWidgetItemViews(): WidgetItemView[] {
+        this.contentWidgetItemView = new ContentWidgetItemView();
+        this.propertiesWidgetItemView = new ArchivePropertiesWidgetItemView();
+
+        return [
+            this.contentWidgetItemView,
+            new ArchiveStatusWidgetItemView(),
+            this.propertiesWidgetItemView,
+            new AttachmentsWidgetItemView().setContentRootPath(ContentPath.ARCHIVE_ROOT)
+        ];
+    }
+
+    setArchiveItem(item: ArchiveContentViewItem) {
+        this.archiveItem = item;
+
+        if (item) {
+            this.contentWidgetItemView.getViewer().getNamesAndIconView().getNamesView().setSubName(item.getOriginalFullPath());
+        }
+    }
+}
