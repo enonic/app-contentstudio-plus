f296b9198d7f128a6f34e4ca448d2e1c5ea12397
Aliaksandr Shklianko <asl@enonic.com>
2021-11-22 15:54:51 +0300

    "Restore content" dialog is missing progress bar #298

--- src/main/resources/assets/js/ArchiveRestoreDialog.ts
+++ src/main/resources/assets/js/ArchiveRestoreDialog.ts
@@ -3,16 +3,41 @@ import {i18n} from 'lib-admin-ui/util/Messages';
 import {RestoreArchivedRequest} from './resource/RestoreArchivedRequest';
 import {DefaultErrorHandler} from 'lib-admin-ui/DefaultErrorHandler';
 import {NotifyManager} from 'lib-admin-ui/notify/NotifyManager';
+import {ProgressBarManager} from 'lib-contentstudio/app/dialog/ProgressBarManager';
+import {TaskId} from 'lib-admin-ui/task/TaskId';
 
 export class ArchiveRestoreDialog
     extends ArchiveDialog {
 
     private static INSTANCE: ArchiveRestoreDialog;
 
+    progressManager: ProgressBarManager;
+
     constructor() {
         super({
             title: i18n('dialog.restore.archive.title'),
             class: 'archive-restore-dialog'
+        });
+    }
+
+    protected initElements(): void {
+        super.initElements();
+
+        this.progressManager = new ProgressBarManager({
+            managingElement: <any>this,
+            processingLabel: i18n('field.progress.restoring')
+        });
+    }
+
+    protected initListeners(): void {
+        super.initListeners();
+
+        this.progressManager.onProgressComplete(() => {
+            const successMessage: string =
+                this.totalToProcess > 1 ?
+                i18n('notify.restored.success.multiple', this.totalToProcess) :
+                i18n('notify.restored.success.single', this.items[0].getDisplayName());
+            NotifyManager.get().showSuccess(successMessage);
         });
     }
 
@@ -28,17 +53,14 @@ export class ArchiveRestoreDialog
         return i18n('action.restore');
     }
 
+    protected executeAction() {
+        this.doAction();
+    }
+
     protected doAction() {
-        new RestoreArchivedRequest(this.items.map(item => item.getId()))
-            .sendAndParseWithPolling()
-            .then(() => {
-                const successMessage: string =
-                    this.totalToProcess > 1 ?
-                    i18n('notify.restored.success.multiple', this.totalToProcess) :
-                    i18n('notify.restored.success.single', this.items[0].getDisplayName() );
-                NotifyManager.get().showSuccess(successMessage);
-            })
-            .catch(DefaultErrorHandler.handle);
+        new RestoreArchivedRequest(this.items.map(item => item.getId())).sendAndParse().then((taskId: TaskId) => {
+            this.progressManager.pollTask(taskId);
+        }).catch(DefaultErrorHandler.handle);
     }
 
     protected getConfirmValueDialogTitle(): string {
