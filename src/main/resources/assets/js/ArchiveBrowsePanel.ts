95fd47cfd32f5c1a0d658671edc9f0a50bd0da1f
Aliaksandr Shklianko <asl@enonic.com>
2021-11-29 14:25:22 +0300

    Mobile mode: adjust classes and styles with recent app-cs changes #314
    -using common classes and behaviour

--- src/main/resources/assets/js/ArchiveBrowsePanel.ts
+++ src/main/resources/assets/js/ArchiveBrowsePanel.ts
@@ -1,25 +1,20 @@
 import {BrowseItemPanel} from 'lib-admin-ui/app/browse/BrowseItemPanel';
-import {BrowsePanel} from 'lib-admin-ui/app/browse/BrowsePanel';
 import {Toolbar} from 'lib-admin-ui/ui/toolbar/Toolbar';
 import {ArchiveTreeGrid} from './ArchiveTreeGrid';
 import {ArchiveBrowseItemPanel} from './ArchiveBrowseItemPanel';
 import * as Q from 'q';
-import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
-import {SplitPanel} from 'lib-admin-ui/ui/panel/SplitPanel';
-import {ContextSplitPanel} from 'lib-contentstudio/app/view/context/ContextSplitPanel';
-import {DockedContextPanel} from 'lib-contentstudio/app/view/context/DockedContextPanel';
 import {ArchiveContextView} from './ArchiveContextView';
 import {ArchiveContentViewItem} from './ArchiveContentViewItem';
 import {NonMobileContextPanelToggleButton} from 'lib-contentstudio/app/view/context/button/NonMobileContextPanelToggleButton';
+import {ResponsiveBrowsePanel} from 'lib-contentstudio/app/browse/ResponsiveBrowsePanel';
+import {ResponsiveToolbar} from 'lib-contentstudio/app/browse/ResponsiveToolbar';
 
 export class ArchiveBrowsePanel
-    extends BrowsePanel {
+    extends ResponsiveBrowsePanel {
 
     protected treeGrid: ArchiveTreeGrid;
 
-    private contextView: ArchiveContextView;
-
-    private contextSplitPanel: ContextSplitPanel;
+    protected contextView: ArchiveContextView;
 
     protected initElements(): void {
         super.initElements();
@@ -28,42 +23,13 @@ export class ArchiveBrowsePanel
         this.browseToolbar.appendChild(new NonMobileContextPanelToggleButton());
     }
 
-    protected initListeners(): void {
-        super.initListeners();
-
-        this.contextSplitPanel.onMobileModeChanged((isMobile: boolean) => {
-            if (isMobile) {
-                this.gridAndItemsSplitPanel.hideSecondPanel();
-            } else {
-                this.gridAndItemsSplitPanel.showFirstPanel();
-                this.gridAndItemsSplitPanel.showSecondPanel();
-            }
-        });
-    }
-
-    protected updatePreviewItem(): void {
-        super.updatePreviewItem();
-
-        const item: ArchiveContentViewItem = <ArchiveContentViewItem>this.treeGrid.getLastSelectedOrHighlightedItem();
-        const summary: ContentSummaryAndCompareStatus = item?.getData();
-
-        this.contextView.setItem(summary);
+    protected updateContextView(item: ArchiveContentViewItem) {
+        this.contextView.setItem(item?.getData());
         this.contextView.setArchiveItem(item);
-
-        if (this.treeGrid.hasHighlightedNode()) {
-            if (this.contextSplitPanel.isMobileMode()) {
-                this.gridAndItemsSplitPanel.hideFirstPanel();
-                this.gridAndItemsSplitPanel.showSecondPanel();
-            }
-        }
     }
 
     protected createTreeGrid(): ArchiveTreeGrid {
         return new ArchiveTreeGrid();
-    }
-
-    protected createToolbar(): Toolbar {
-        return new Toolbar();
     }
 
     protected createBrowseItemPanel(): BrowseItemPanel {
@@ -78,24 +44,11 @@ export class ArchiveBrowsePanel
         });
     }
 
-    protected createBrowseWithItemsPanel(): SplitPanel {
-        this.contextView = new ArchiveContextView();
-
-        this.contextSplitPanel = ContextSplitPanel.create(this.getBrowseItemPanel(),
-            new DockedContextPanel(this.contextView))
-            .setContextView(this.contextView)
-            .build();
-
-        this.contextSplitPanel.onFoldClicked(() => {
-            this.gridAndItemsSplitPanel.showFirstPanel();
-            this.gridAndItemsSplitPanel.showFirstPanel();
-            this.gridAndItemsSplitPanel.hideSecondPanel();
-        });
-
-        return this.contextSplitPanel;
-    }
-
     protected togglePreviewPanelDependingOnScreenSize(): void {
         //
+    }
+
+    protected createContextView(): ArchiveContextView {
+        return new ArchiveContextView();
     }
 }
