72c37cfffd72e156bdac25a5beaf34c7e8d93582
Siarhei Hauruseu <sig@enonic.com>
2021-12-13 19:10:36 +0300

    ui-tests issue#362

---
+++ testing/page_objects/archive/archived.content.versions.widget.js
@@ -0,0 +1,38 @@
+/**
+ * Created on 08.12.2021
+ */
+const BaseVersionsWidget = require('../details_panel/base.versions.widget');
+const appConst = require('../../libs/app_const');
+const lib = require('../../libs/elements');
+
+const XPATH = {
+    widget: "//div[contains(@id,'ArchiveBrowsePanel')]//div[contains(@id,'VersionHistoryView')]",
+    versionsList: "//ul[contains(@id,'VersionHistoryList')]",
+    versionItem: "//li[contains(@class,'version-list-item') and child::div[contains(@class,'version-viewer')]]",
+    archivedItems: "//li[contains(@id,'VersionHistoryListItem')and descendant::h6[contains(.,'Archived')]]",
+    restoredItems: "//li[contains(@id,'VersionHistoryListItem')and descendant::h6[contains(.,'Restored')]]",
+};
+
+class ArchivedContentVersionsWidget extends BaseVersionsWidget {
+
+    get versionsWidget() {
+        return XPATH.widget;
+    }
+
+    get versionItems() {
+        return this.versionsWidget + XPATH.versionsList + XPATH.versionItem;
+    }
+
+    get archivedItems() {
+        return this.versionsWidget + XPATH.versionsList + XPATH.archivedItems;
+    }
+
+    async getStatusInArchivedItem(index) {
+        let locator = XPATH.widget + XPATH.archivedItems + lib.H6_DISPLAY_NAME;
+        this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+        let elements = await this.findElements(locator);
+        return await elements[index].getText();
+    }
+}
+
+module.exports = ArchivedContentVersionsWidget;
--- testing/specs/archive.context.panel.spec.js
+++ testing/specs/archive.context.panel.spec.js
@@ -14,6 +14,10 @@ const ConfirmValueDialog = require('../page_objects/confirm.content.delete.dialo
 const ArchivedContentPropertiesWidget = require('../page_objects/archive/archived.content.properties.widget');
 const ArchivedContentStatusWidget = require('../page_objects/archive/archived.content.status.widget');
 const ArchiveBrowseContextPanel = require('../page_objects/archive/archive.browse.context.panel');
+const ArchivedContentVersionsWidget = require('../page_objects/archive/archived.content.versions.widget');
+const ContentBrowseDetailsPanel = require('../page_objects/browsepanel/detailspanel/browse.details.panel');
+const BrowseVersionsWidget = require('../page_objects/browsepanel/detailspanel/browse.versions.widget');
+const ArchiveRestoreDialog = require('../page_objects/archive/archive.restore.dialog');
 
 describe('archive.context.panel.spec: tests for archive context panel', function () {
     this.timeout(appConst.SUITE_TIMEOUT);
@@ -50,6 +54,11 @@ describe('archive.context.panel.spec: tests for archive context panel', function
             //3. Verify the properties widget
             let actualOwner = await archivedContentPropertiesWidget.getOwner();
             assert.equal(actualOwner, "su", "Expected owner should be displayed");
+            let actualType = await archivedContentPropertiesWidget.getType();
+            assert.equal(actualType, "folder", "Expected type should be displayed");
+            let actualApplication = await archivedContentPropertiesWidget.getApplication();
+            assert.equal(actualApplication, "base", "Expected application should be displayed");
+
             //4. Verify the status widget
             let actualStatus = await archivedContentStatusWidget.getStatus();
             assert.equal(actualStatus, "Archived", "Expected status should be displayed");
@@ -68,6 +77,53 @@ describe('archive.context.panel.spec: tests for archive context panel', function
             assert.isTrue(actualOptions.includes('Details'));
             assert.isTrue(actualOptions.includes('Version history'));
             assert.equal(actualOptions.length, 2, "Two options should be in the selector");
+        });
+
+    it(`GIVEN archived content has been selected WHEN 'Versions widget' has been opened THEN expected Archived version item should be displayed in the widget`,
+        async () => {
+            let archiveBrowseContextPanel = new ArchiveBrowseContextPanel();
+            let archiveBrowsePanel = new ArchiveBrowsePanel();
+            let archivedContentVersionsWidget = new ArchivedContentVersionsWidget();
+            await studioUtils.openArchivePanel();
+            //1. Select the archived content
+            await archiveBrowsePanel.clickCheckboxAndSelectRowByDisplayName(FOLDER1.displayName);
+            //2. Open the versions widget:
+            await archiveBrowseContextPanel.openVersionHistory();
+            //3. Verify that 'archived' version item appears in the widget:
+            let actualStatus = await archivedContentVersionsWidget.getStatusInArchivedItem(0);
+            assert.isTrue(actualStatus.includes("Archived"));
+            let archivedBy = await archivedContentVersionsWidget.getArchivedBy(0);
+            assert.equal(archivedBy, "by Super User", "Expected user should be displayed");
+        });
+
+    it(`GIVEN restored content has been selected WHEN 'Versions widget' has been opened THEN expected Restored version item should be displayed in the widget`,
+        async () => {
+            let archiveBrowsePanel = new ArchiveBrowsePanel();
+            let contentBrowsePanel = new ContentBrowsePanel();
+            let archiveRestoreDialog = new ArchiveRestoreDialog();
+            let contentBrowseDetailsPanel = new ContentBrowseDetailsPanel();
+            let browseVersionsWidget = new BrowseVersionsWidget();
+            await studioUtils.openArchivePanel();
+            //1. Select the archived content
+            await archiveBrowsePanel.clickCheckboxAndSelectRowByDisplayName(FOLDER1.displayName);
+            //2. Restore the content
+            await archiveBrowsePanel.clickOnRestoreButton();
+            await archiveRestoreDialog.waitForOpened();
+            await studioUtils.saveScreenshot("folder_to_restore1_3");
+            await archiveRestoreDialog.clickOnRestoreButton();
+            await contentBrowsePanel.waitForNotificationMessage();
+            //3. Go to Content Browse Panel:
+            await studioUtils.switchToContentMode();
+            //4. Select the folder and open Versions History:
+            await studioUtils.findAndSelectItem(FOLDER1.displayName);
+            await contentBrowseDetailsPanel.openVersionHistory();
+            //5. Verify that new 'Restored' version-item appears in the widget:
+            let actualUser = await browseVersionsWidget.getRestoredBy(0);
+            assert.equal(actualUser, "by Super User", "Expected user should be displayed in the 'restored by'");
+            //6. Verify the status in the restored content
+            let status = await browseVersionsWidget.getContentStatus();
+            //Verify the content's status in the top of Version Widget
+            assert.equal(status, appConst.CONTENT_STATUS.NEW, "'New' status should be after the restoring this content");
         });
 
 
