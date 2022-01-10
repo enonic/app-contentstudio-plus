4852e618074d5779ec5e6e7db5cc3d1a26c92eb7
Siarhei Hauruseu <sig@enonic.com>
2022-01-10 09:35:50 +0300

    update failed tests

--- testing/specs/archive.browse.panel.spec.js
+++ testing/specs/archive.browse.panel.spec.js
@@ -106,6 +106,24 @@ describe('archive.browse.panel.spec: tests for archive browse panel and selectio
             assert.equal(actualDisplayName, FOLDER1.displayName, "Expected content name should be displayed on thr panel");
         });
 
+    it("GIVEN a folder is selected WHEN the folder has been unselected THEN Item Statistics panel should be cleared",
+        async () => {
+            //1. Navigate to 'Archive Browse Panel' and click on a checkbox of a archived folder:
+            await studioUtils.openArchivePanel();
+            let archiveBrowsePanel = new ArchiveBrowsePanel();
+            let archiveItemStatisticsPanel = new ArchiveItemStatisticsPanel();
+            let contentWidgetItemView = new ArchiveContentWidgetItemView();
+            //1. Click on the row and select an item:
+            await archiveBrowsePanel.clickOnRowByDisplayName(FOLDER1.displayName);
+            //2. Verify the path and status of the content:
+            let path = await archiveItemStatisticsPanel.getPath();
+            assert.equal(path, `/${FOLDER1.displayName}`, "Expected path should be displayed in Item Statistics panel");
+            //3. Click on he row and unselect the item
+            await archiveBrowsePanel.clickOnRowByDisplayName(FOLDER1.displayName);
+            //4. Verify that Item Statistics panel is cleared:
+            await archiveItemStatisticsPanel.waitForPanelCleared();
+        });
+
     it(`GIVEN 2 folders are selected AND selection controller has been clicked WHEN both folders have been deleted THEN selection controller(circle) gets not visible`,
         async () => {
             let archiveDeleteDialog = new ArchiveDeleteDialog();
@@ -119,8 +137,8 @@ describe('archive.browse.panel.spec: tests for archive browse panel and selectio
             //3. Click on Delete... button in the browse toolbar
             await archiveBrowsePanel.clickOnDeleteButton();
             await archiveDeleteDialog.waitForOpened();
-            //4. Click on 'Delete Now' button in the modal dialog:
-            await archiveDeleteDialog.clickOnDeleteNowButton();
+            //4. Click on 'Delete' button in the modal dialog:
+            await archiveDeleteDialog.clickOnDeleteButton();
             await confirmValueDialog.waitForDialogOpened();
             //5. Confirm the value:
             await confirmValueDialog.typeNumberOrName(2);
