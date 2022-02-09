f023b7576c805f1efbfd8dd7196d2bfbaeb9c0ae
Alan Semenov <ase@enonic.com>
2022-02-09 23:26:25 +0100

    Comply with the latest changes in IsRenderableRequest (#430)

--- src/main/resources/assets/js/ArchiveItemPreviewPanel.ts
+++ src/main/resources/assets/js/ArchiveItemPreviewPanel.ts
@@ -1,12 +1,10 @@
 import {ArchiveItemPreviewToolbar} from './ArchiveItemPreviewToolbar';
 import {IsRenderableRequest} from 'lib-contentstudio/app/resource/IsRenderableRequest';
 import {DefaultErrorHandler} from 'lib-admin-ui/DefaultErrorHandler';
-import {ArchiveResourceRequest} from './resource/ArchiveResourceRequest';
 import {ContentPath} from 'lib-contentstudio/app/content/ContentPath';
 import {ArchiveContentViewItem} from './ArchiveContentViewItem';
 import {ContentItemPreviewPanel} from 'lib-contentstudio/app/view/ContentItemPreviewPanel';
 import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
-
 
 export class ArchiveItemPreviewPanel
     extends ContentItemPreviewPanel {
@@ -27,8 +25,7 @@ export class ArchiveItemPreviewPanel
         if (this.renderableItems.has(item.getId())) {
             this.debouncedSetItem(item);
         } else {
-            new IsRenderableRequest(item.getData().getContentId())
-                .setContentRootPath(ArchiveResourceRequest.ARCHIVE_PATH)
+            new IsRenderableRequest(item.getData().getContentSummary())
                 .sendAndParse()
                 .then((isRenderable: boolean) => {
                     item.getData().setRenderable(isRenderable);
