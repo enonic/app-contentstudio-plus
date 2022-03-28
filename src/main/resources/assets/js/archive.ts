f36c1e0de2b90e226eab95ea76a0697d0610b46b
Aliaksandr Shklianko <asl@enonic.com>
2022-03-28 10:46:13 +0300

    Change Content Studio+ to be a widget #472

---
+++ src/main/resources/assets/js/archive.ts
@@ -0,0 +1,49 @@
+import {i18nAdd} from 'lib-admin-ui/util/MessagesInitializer';
+import {Body} from 'lib-admin-ui/dom/Body';
+import {Element} from 'lib-admin-ui/dom/Element';
+import {CONFIG} from 'lib-admin-ui/util/Config';
+import {ArchiveAppContainer} from './ArchiveAppContainer';
+
+const injectApp = (widgetElem: Element): void => {
+    const archiveAppContainer: ArchiveAppContainer = new ArchiveAppContainer();
+    widgetElem.appendChild(archiveAppContainer);
+    archiveAppContainer.show();
+
+    widgetElem.onShown(() => {
+        archiveAppContainer.show(); // need to toggle key bindings
+    });
+
+    widgetElem.onHidden(() => {
+        archiveAppContainer.hide(); // need to toggle key bindings
+    });
+};
+
+const init = async (configServiceUrl: string, i18nServiceUrl: string): Promise<void> => {
+    await i18nAdd(i18nServiceUrl);
+    await CONFIG.init(configServiceUrl);
+};
+
+void (async (currentScript: HTMLOrSVGScriptElement) => {
+    if (!currentScript) {
+        throw 'Legacy browsers are not supported';
+    }
+
+    const configServiceUrl = currentScript.getAttribute('configServiceUrl');
+    const i18nServiceUrl = currentScript.getAttribute('i18nServiceUrl');
+    const elemId: string = currentScript.getAttribute('elemId');
+
+    if (!configServiceUrl || !i18nServiceUrl || !elemId) {
+        throw 'Missing attributes on inject script';
+    }
+
+    await init(configServiceUrl, i18nServiceUrl);
+
+    const body: Body = Body.get();
+    const widgetEl: Element = body.findChildById(elemId, true);
+
+    const renderListener = (): void => {
+        injectApp(widgetEl);
+        body.unRendered(renderListener);
+    };
+    body.whenRendered(renderListener);
+})(document.currentScript);
