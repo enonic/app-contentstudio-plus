5b07bbd8b9ddf24456fe07e09b6b74d0b2e4bd06
Aliaksandr Shklianko <asl@enonic.com>
2022-05-23 12:01:04 +0300

    -refactoring

--- src/main/resources/assets/js/ArchiveFilterPanel.ts
+++ src/main/resources/assets/js/ArchiveFilterPanel.ts
@@ -4,13 +4,8 @@ import {DefaultErrorHandler} from 'lib-admin-ui/DefaultErrorHandler';
 import {i18n} from 'lib-admin-ui/util/Messages';
 import {ContentAggregation} from 'lib-contentstudio/app/browse/filter/ContentAggregation';
 import {ContentQuery} from 'lib-contentstudio/app/content/ContentQuery';
-import {ContentSummary} from 'lib-contentstudio/app/content/ContentSummary';
-import {ContentSummaryJson} from 'lib-contentstudio/app/content/ContentSummaryJson';
-import {ContentQueryRequest} from 'lib-contentstudio/app/resource/ContentQueryRequest';
-import {ContentQueryResult} from 'lib-contentstudio/app/resource/ContentQueryResult';
 import {ArchiveViewItem} from './ArchiveViewItem';
 import {ArchiveResourceRequest} from './resource/ArchiveResourceRequest';
-import {Expand} from 'lib-admin-ui/rest/Expand';
 import * as Q from 'q';
 import {Aggregation} from 'lib-admin-ui/aggregation/Aggregation';
 import {LoginResult} from 'lib-admin-ui/security/auth/LoginResult';
@@ -18,9 +13,10 @@ import {IsAuthenticatedRequest} from 'lib-admin-ui/security/auth/IsAuthenticated
 import {BucketAggregation} from 'lib-admin-ui/aggregation/BucketAggregation';
 import {Bucket} from 'lib-admin-ui/aggregation/Bucket';
 import {ArchiveAggregation} from './ArchiveAggregation';
-import {ArchiveSearchContentQueryCreator} from './ArchiveSearchContentQueryCreator';
 import {ArchiveAggregationsDisplayNamesResolver} from './ArchiveAggregationsDisplayNamesResolver';
 import {FilterableAggregationGroupView} from 'lib-contentstudio/app/browse/filter/FilterableAggregationGroupView';
+import {AggregationsQueryResult} from 'lib-contentstudio/app/browse/filter/AggregationsQueryResult';
+import {ArchiveAggregationsFetcher} from './ArchiveAggregationsFetcher';
 
 export class ArchiveFilterPanel
     extends BrowseFilterPanel<ArchiveViewItem> {
@@ -28,6 +24,8 @@ export class ArchiveFilterPanel
     private aggregations: Map<string, AggregationGroupView>;
 
     private aggregationsDisplayNamesResolver: ArchiveAggregationsDisplayNamesResolver;
+
+    private aggregationsFetcher: ArchiveAggregationsFetcher;
 
     private searchEventListeners: { (query?: ContentQuery): void; }[] = [];
 
@@ -37,6 +35,8 @@ export class ArchiveFilterPanel
         super();
 
         this.aggregationsDisplayNamesResolver = new ArchiveAggregationsDisplayNamesResolver();
+        this.aggregationsFetcher = new ArchiveAggregationsFetcher(this.getAggregationsList());
+        this.aggregationsFetcher.setRootPath(ArchiveResourceRequest.ARCHIVE_PATH);
         this.initAggregationGroupView();
     }
 
@@ -47,6 +47,24 @@ export class ArchiveFilterPanel
     unSearchEvent(listener: { (query?: ContentQuery): void; }): void {
         this.searchEventListeners = this.searchEventListeners.filter((curr: { (query?: ContentQuery): void; }) => {
             return curr !== listener;
+        });
+    }
+
+    doSearch(): Q.Promise<void> {
+        if (this.hasFilterSet()) {
+            return this.getAndUpdateAggregations().then(() => {
+                this.notifySearchEvent(this.aggregationsFetcher.createContentQuery(this.getSearchInputValues()));
+                return Q.resolve();
+            });
+        }
+
+        return this.resetFacets();
+    }
+
+    protected resetFacets(_suppressEvent?: boolean, _doResetAll?: boolean): Q.Promise<void> {
+        return this.getAndUpdateAggregations().then(() => {
+            this.notifySearchEvent();
+            return Q.resolve();
         });
     }
 
@@ -71,24 +89,30 @@ export class ArchiveFilterPanel
         return Array.from(this.aggregations.values());
     }
 
-    protected doSearch(): Q.Promise<void> {
-        if (this.hasFilterSet()) {
-            const query: ContentQuery = this.buildQuery();
-
-            return this.searchDataAndHandleResponse(query).then(() => {
-                this.notifySearchEvent(query);
-                return Q.resolve();
+    private getAndUpdateAggregations(): Q.Promise<AggregationsQueryResult> {
+        return this.getAggregations().then((aggregationsQueryResult: AggregationsQueryResult) => {
+            this.updateHitsCounter(aggregationsQueryResult.getMetadata().getTotalHits());
+            return this.processAggregations(aggregationsQueryResult.getAggregations()).then(() => {
+                return aggregationsQueryResult;
             });
-        }
-
-        return this.resetFacets();
+        });
     }
 
-    protected resetFacets(_suppressEvent?: boolean, _doResetAll?: boolean): Q.Promise<void> {
-        return this.searchDataAndHandleResponse(this.buildQuery()).then(() => {
-            this.notifySearchEvent();
-            return Q.resolve();
-        });
+    private processAggregations(aggregations: Aggregation[]): Q.Promise<void> {
+        this.toggleAggregationsVisibility(aggregations);
+
+        return this.aggregationsDisplayNamesResolver.updateAggregationsDisplayNames(aggregations, this.getCurrentUserKeyAsString()).then(
+            () => {
+                this.updateAggregations(aggregations);
+                return Q.resolve();
+            });
+    }
+
+    private getAggregations(): Q.Promise<AggregationsQueryResult> {
+        this.aggregationsFetcher.setSearchInputValues(this.getSearchInputValues());
+        this.aggregationsFetcher.setConstraintItemsIds(this.hasConstraint() ? this.getSelectionItems() : null);
+
+        return this.aggregationsFetcher.getAggregations();
     }
 
     private initAggregationGroupView(): void {
@@ -98,52 +122,13 @@ export class ArchiveFilterPanel
                 [this.getCurrentUserKeyAsString()]);
             (<FilterableAggregationGroupView>this.aggregations.get(ArchiveAggregation.ARCHIVED_BY)).setIdsToKeepOnToTop(
                 [this.getCurrentUserKeyAsString()]);
-            this.searchDataAndHandleResponse(this.buildQuery()).catch(DefaultErrorHandler.handle);
 
-            return Q.resolve();
+            return this.getAndUpdateAggregations();
         }).catch(DefaultErrorHandler.handle);
-    }
-
-    private searchDataAndHandleResponse(contentQuery: ContentQuery): Q.Promise<void> {
-        return this.sendContentQueryRequest(contentQuery).then(
-            (contentQueryResult: ContentQueryResult<ContentSummary, ContentSummaryJson>) => {
-                this.processSearchResponse(contentQueryResult);
-                return Q.resolve();
-            })
-            .catch(DefaultErrorHandler.handle);
-    }
-
-    private sendContentQueryRequest(contentQuery: ContentQuery): Q.Promise<ContentQueryResult<ContentSummary, ContentSummaryJson>> {
-        return new ContentQueryRequest<ContentSummaryJson, ContentSummary>(contentQuery)
-            .setExpand(Expand.SUMMARY)
-            .setContentRootPath(ArchiveResourceRequest.ARCHIVE_PATH)
-            .sendAndParse();
-    }
-
-    private processSearchResponse(contentQueryResult: ContentQueryResult<ContentSummary, ContentSummaryJson>): void {
-        const aggregations: Aggregation[] = contentQueryResult.getAggregations();
-        this.updateAggregations(aggregations);
-        this.aggregationsDisplayNamesResolver.updateAggregationsDisplayNames(aggregations, this.getCurrentUserKeyAsString()).then(() => {
-            this.updateAggregations(aggregations);
-            return Q.resolve();
-        }).catch(DefaultErrorHandler.handle);
-        this.toggleAggregationsVisibility(aggregations);
-        this.updateHitsCounter(contentQueryResult.getMetadata().getTotalHits());
     }
 
     private getCurrentUserKeyAsString(): string {
         return this.userInfo.getUser().getKey().toString();
-    }
-
-    private buildQuery(): ContentQuery {
-        const queryCreator: ArchiveSearchContentQueryCreator = new ArchiveSearchContentQueryCreator(this.getSearchInputValues());
-
-        queryCreator.setIsAggregation(true);
-        queryCreator.setConstraintItemsIds(this.hasConstraint() ? this.getSelectionItems() : null);
-
-        return queryCreator.create(
-            [ContentAggregation.CONTENT_TYPE, ArchiveAggregation.ARCHIVED, ArchiveAggregation.ARCHIVED_BY, ContentAggregation.OWNER,
-                ContentAggregation.LANGUAGE]);
     }
 
     private notifySearchEvent(query?: ContentQuery): void {
@@ -157,5 +142,9 @@ export class ArchiveFilterPanel
             const isAggregationVisible: boolean = aggregation.getBuckets().some((bucket: Bucket) => bucket.docCount > 0);
             this.aggregations.get(aggregation.getName()).setVisible(isAggregationVisible);
         });
+    }
+
+    private getAggregationsList(): string[] {
+        return Array.from(this.aggregations.keys());
     }
 }
