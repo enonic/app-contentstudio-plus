08b41972af66fdee42cd96043d7ebb5cbc4bf79b
ashklianko <asl@enonic.com>
2023-03-03 14:09:04 +0100

    Inherited content is not localized after clicking on Localize button in widget #823
    Duplicate action opens Archive tab #811

--- src/main/resources/assets/js/widgets/ArchivePropertiesWidgetItemView.ts
+++ src/main/resources/assets/js/widgets/ArchivePropertiesWidgetItemView.ts
@@ -1,24 +1,18 @@
-import {PropertiesWidgetItemView} from 'lib-contentstudio/app/view/context/widget/details/PropertiesWidgetItemView';
-import {Application} from '@enonic/lib-admin-ui/application/Application';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
-import {GetContentVersionsRequest} from 'lib-contentstudio/app/resource/GetContentVersionsRequest';
-import {ContentVersions} from 'lib-contentstudio/app/ContentVersions';
-import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {Element} from '@enonic/lib-admin-ui/dom/Element';
 import {DateTimeFormatter} from '@enonic/lib-admin-ui/ui/treegrid/DateTimeFormatter';
+import {BasePropertiesWidgetItemView} from 'lib-contentstudio/app/view/context/widget/details/BasePropertiesWidgetItemView';
 
 
 export class ArchivePropertiesWidgetItemView
-    extends PropertiesWidgetItemView {
+    extends BasePropertiesWidgetItemView {
 
-    protected layoutApplication(application?: Application): void {
-        super.layoutApplication(application);
+    protected layoutProperties(): void {
+        super.layoutProperties();
 
-        new GetContentVersionsRequest(this.item.getContentId()).sendAndParse().then((versions: ContentVersions) => {
-            const archived: string = i18n('status.archived');
-            const archivedDate: string = DateTimeFormatter.createHtml(this.item.getContentSummary().getArchivedTime());
-            this.insertKeyValue(archived, archivedDate, this.getIndexToInsertArchived());
-        }).catch(DefaultErrorHandler.handle);
+        const archived: string = i18n('status.archived');
+        const archivedDate: string = DateTimeFormatter.createHtml(this.item.getContentSummary()?.getArchivedTime());
+        this.insertKeyValue(archived, archivedDate, this.getIndexToInsertArchived());
     }
 
     private getIndexToInsertArchived(): number {
@@ -30,5 +24,9 @@ export class ArchivePropertiesWidgetItemView
         }
 
         return this.list.getChildren().length;
+    }
+
+    protected layoutEditLink(): void {
+        //
     }
 }
--- testing/page_objects/browsepanel/detailspanel/properties.widget.itemview.js
+++ testing/page_objects/browsepanel/detailspanel/properties.widget.itemview.js
@@ -6,12 +6,37 @@ const appConst = require('../../../libs/app_const');
 
 const xpath = {
     container: `//div[contains(@id,'WidgetView')]//div[contains(@id,'PropertiesWidgetItemView')]`,
-    languageProperty: "//dd[contains(.,'Language:')]/following-sibling::dt[1]",
-    ownerProperty: "//dd[contains(.,'Owner:')]/following-sibling::dt[1]",
-    publishFromProperty: "//dd[contains(.,'Publish From:')]/following-sibling::dt[1]",
+    languageProperty: "//dd[contains(.,'Language')]/following-sibling::dt[1]",
+    ownerProperty: "//dd[contains(.,'Owner')]/following-sibling::dt[1]",
+    publishFromProperty: "//dd[contains(.,'Publish From')]/following-sibling::dt[1]",
+    applicationProperty: "//dd[contains(.,'Application')]/following-sibling::dt[1]",
+    type: "//dd[contains(.,'Type')]/following-sibling::dt[1]",
+    firstPublished: "//dd[contains(.,'First Published')]/following-sibling::dt[1]",
+    modified: "//dd[contains(.,'Modified')]/following-sibling::dt[1]",
+    editSettingsButton: "//a[contains(@class,'edit-settings-link') and text()='Edit Settings']",
 };
 
 class PropertiesItemView extends Page {
+
+    get applicationProperty() {
+        return xpath.container + xpath.applicationProperty;
+    }
+
+    get editSettingsButton() {
+        return xpath.container + xpath.editSettingsButton;
+    }
+
+    get typeProperty() {
+        return xpath.container + xpath.type;
+    }
+
+    get modifiedProperty() {
+        return xpath.container + xpath.modified;
+    }
+
+    get firstPublishedProperty() {
+        return xpath.container + xpath.firstPublished;
+    }
 
     get languageProperty() {
         return xpath.container + xpath.languageProperty;
@@ -25,14 +50,33 @@ class PropertiesItemView extends Page {
         return xpath.container + xpath.publishFromProperty;
     }
 
+    waitForEditSettingsButtonDisplayed() {
+        return this.waitForElementDisplayed(this.editSettingsButton, appConst.mediumTimeout);
+    }
+
+    waitForEditSettingsButtonNotDisplayed() {
+        return this.waitForElementNotDisplayed(this.editSettingsButton, appConst.mediumTimeout);
+    }
+
+    async clickOnEditSettingsButton() {
+        try {
+            await this.waitForEditSettingsButtonDisplayed();
+            await this.clickOnElement(this.editSettingsButton);
+            await this.pause(300);
+        } catch (err) {
+            let screenshot = appConst.generateRandomName('prop_widget_edit');
+            await this.saveScreenshot(screenshot);
+            throw new Error(`Properties Widget, Edit button is not displayed ${screenshot} ` + err);
+        }
+    }
+
     async waitForLanguageVisible() {
         try {
-            await this.waitForElementDisplayed(this.languageProperty, appConst.shortTimeout);
+            await this.waitForElementDisplayed(this.languageProperty, appConst.mediumTimeout);
         } catch (err) {
-            //Workaround for the issue with empty details panel in Wizard
-            await this.refresh();
-            await this.pause(2000);
-            await this.waitForElementDisplayed(this.languageProperty, appConst.shortTimeout);
+            let screenshot = appConst.generateRandomName('prop_widget_lang');
+            await this.saveScreenshot(screenshot);
+            throw new Error(`Properties Widget, language is not displayed ${screenshot} ` + err);
         }
     }
 
@@ -44,13 +88,23 @@ class PropertiesItemView extends Page {
 
     waitForOwnerNotVisible() {
         return this.waitForElementNotDisplayed(this.ownerProperty, appConst.shortTimeout).catch(err => {
-            throw new Error("Owner should not be present in the properties widget! " + err);
+            throw new Error('Owner should not be present in the properties widget! ' + err);
         });
     }
 
     async getLanguage() {
         await this.waitForLanguageVisible();
         return await this.getText(this.languageProperty);
+    }
+
+    async getApplication() {
+        await this.waitForElementDisplayed(this.applicationProperty, appConst.mediumTimeout);
+        return await this.getText(this.applicationProperty);
+    }
+
+    async getType() {
+        await this.waitForElementDisplayed(this.typeProperty, appConst.mediumTimeout);
+        return await this.getText(this.typeProperty);
     }
 
     async getPublishFrom() {
@@ -62,14 +116,24 @@ class PropertiesItemView extends Page {
         try {
             await this.waitForElementDisplayed(this.ownerProperty, appConst.shortTimeout);
         } catch (err) {
-            //Workaround for the issue with empty details panel in Wizard
-            await this.refresh();
-            await this.pause(2000);
-            await this.waitForElementDisplayed(this.ownerProperty, appConst.shortTimeout);
+            let screenshot = appConst.generateRandomName('prop_widget');
+            await this.saveScreenshot(screenshot);
+            throw new Error(`Properties Widget, owner is not displayed ${screenshot} ` + err);
         }
+    }
+
+    async getOwnerName() {
+        await this.waitForOwnerDisplayed();
+        return await this.getText(this.ownerProperty);
+    }
+
+    waitForModifiedDateDisplayed() {
+        return this.waitForElementDisplayed(this.modifiedProperty, appConst.shortTimeout);
+    }
+
+    waitForFirstPublishedDateDisplayed() {
+        return this.waitForElementDisplayed(this.firstPublishedProperty, appConst.shortTimeout);
     }
 }
 
 module.exports = PropertiesItemView;
-
-
