967cda05ae40fdb0fad7e15f57d20281107ec24d
ashklianko <asl@enonic.com>
2023-10-20 14:30:13 +0200

    Publishing Report json contains permissions and inheritPermissions fields #1096

--- src/main/resources/assets/js/widgets/publish-report/dialog/ComparisonHelper.ts
+++ src/main/resources/assets/js/widgets/publish-report/dialog/ComparisonHelper.ts
@@ -1,21 +1,11 @@
 import {ContentJson} from 'lib-contentstudio/app/content/ContentJson';
-import {ContentVersion} from 'lib-contentstudio/app/ContentVersion';
-import {ObjectHelper} from '@enonic/lib-admin-ui/ObjectHelper';
 
 export class ComparisonHelper {
 
-    static processContentJson(contentJson: ContentJson, version: ContentVersion): ContentJson {
+    static processContentJson(contentJson: ContentJson): ContentJson {
         [
-            '_id', 'creator', 'createdTime', 'hasChildren'
+            'hasChildren',
         ].forEach(e => delete contentJson[e]);
-
-        if (ObjectHelper.isDefined(version?.getPermissions())) {
-            contentJson['permissions'] = version.getPermissions().toJson();
-        }
-
-        if (ObjectHelper.isDefined(version?.isInheritPermissions())) {
-            contentJson['inheritPermissions'] = version.isInheritPermissions();
-        }
 
         return contentJson;
     }
