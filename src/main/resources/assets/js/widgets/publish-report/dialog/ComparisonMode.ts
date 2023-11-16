7364fe930ef395623707c54e9cacbd16b83edd00
alansemenov <ase@enonic.com>
2023-11-16 14:21:59 +0100

    Add icons to the Publishing Report widget #1125

--- src/main/resources/assets/js/widgets/publish-report/dialog/ComparisonMode.ts
+++
@@ -1,3 +0,0 @@
-export enum ComparisonMode {
-    COMPARE, DISPLAY_SINGLE
-}
--- src/main/resources/assets/js/widgets/publish-report/dialog/TextAndDateBlock.ts
+++ src/main/resources/assets/js/widgets/publish-report/dialog/TextAndDateBlock.ts
@@ -1,11 +1,24 @@
 import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
 import {SpanEl} from '@enonic/lib-admin-ui/dom/SpanEl';
+import {Element} from '@enonic/lib-admin-ui/dom/Element';
 
 export class TextAndDateBlock
     extends DivEl {
 
+    private readonly iconEl: SpanEl;
+
     constructor(className: string = '') {
         super('text-and-date ' + className);
+
+        this.appendChild(this.iconEl = new SpanEl('state-icon'));
+        this.iconEl.hide();
+    }
+
+    setIconClass(value?: string): this {
+        this.iconEl.setClass('state-icon ' + (value ? value : ''));
+        this.iconEl.setVisible(!!value);
+
+        return this;
     }
 
     addEntry(text: string, dateAsString: string): this {
@@ -14,7 +27,14 @@ export class TextAndDateBlock
     }
 
     setEntry(text: string, dateAsString: string): this {
-        this.removeChildren();
+        this.getChildren().slice(0).forEach((child: Element) => {
+            if (child !== this.iconEl) {
+                child.remove();
+            }
+        });
+
+        this.setIconClass();
+
         this.addEntry(text, dateAsString);
         return this;
     }
