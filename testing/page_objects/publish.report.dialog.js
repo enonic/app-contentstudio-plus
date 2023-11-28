f0c528087269b5fda813de7a8802ec5685bbc686
sgauruseu <sig@eninic.com>
2023-11-28 16:48:35 +0300

    ui-tests issue #1119

--- testing/page_objects/publish.report.dialog.js
+++ testing/page_objects/publish.report.dialog.js
@@ -1,17 +1,21 @@
 /**
- * Created on 03.11.2023
+ * Created on 27.11.2023
  */
 const Page = require('./page');
 const appConst = require('../libs/app_const');
 const lib = require('../libs/elements');
 const XPATH = {
     container: `//div[contains(@id,'PublishReportDialog')]`,
+    comparisonsContainer: "//div[contains(@id,'ComparisonsContainer')]",
+    comparisonBlockDiv: "//div[contains(@id,'ComparisonBlock')]",
+    textAndDateHeader: "//div[@class='header']/div[contains(@id,'TextAndDateBlock')]",
+    textAndDateSubheader: "//div[contains(@id,'TextAndDateBlock') and contains(@class,'subtitle')]",
 };
 
 class PublishReportDialog extends Page {
 
     get showEntireContentCheckbox() {
-        return XPATH.container + lib.SHOW_ENTIRE_CONTENT_CHECKBOX_DIV;
+        return XPATH.container + XPATH.comparisonBlockDiv + lib.SHOW_ENTIRE_CONTENT_CHECKBOX_DIV;
     }
 
     get printButton() {
@@ -22,12 +26,84 @@ class PublishReportDialog extends Page {
         return XPATH.container + lib.CANCEL_BUTTON_TOP;
     }
 
-    async isShowEntireContentCheckboxSelected(index) {
-        let checkBoxInput = this.showEntireContentCheckbox + lib.CHECKBOX_INPUT;
+    async getHeaderInComparisonBlock(index) {
+        try {
+            let locator = XPATH.container + XPATH.comparisonBlockDiv + XPATH.textAndDateHeader + "/span[@class='text'][1]";
+            await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+            let elements = await this.findElements(locator)
+            return await elements[index].getText();
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_comparison_header');
+            throw new Error("PublishReport modal dialog, header block, screenshot:  " + screenshot + ' ' + err);
+        }
+    }
 
+    async getSubtitleInComparisonBlock(index) {
+        try {
+            let locator = XPATH.container + XPATH.comparisonBlockDiv + XPATH.textAndDateSubheader + "/span[@class='text'][1]";
+            await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+            let elements = await this.findElements(locator)
+            return await elements[index].getText();
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_date_subtitle');
+            throw new Error("PublishReport modal dialog, subtitle block, screenshot:  " + screenshot + ' ' + err);
+        }
+    }
+
+    async getDateInHeaderOfComparisonBlock(index) {
+        try {
+            let locator = XPATH.container + XPATH.comparisonBlockDiv + XPATH.textAndDateHeader + "/span[@class='date']";
+            await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+            let elements = await this.findElements(locator)
+            return await elements[index].getText();
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_date_header');
+            throw new Error("PublishReport modal dialog, date in the header block, screenshot:  " + screenshot + ' ' + err);
+        }
+    }
+
+    async getAllComparisonsBlockHeader() {
+        try {
+            let locator = XPATH.comparisonsContainer + "/div[contains(@id,'TextAndDateBlock')]" + "/span[@class='text']";
+            await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+            return await this.getText(locator);
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_dlg_header');
+            throw new Error("PublishReport modal dialog, text in  TextAndDate Block, screenshot:  " + screenshot + ' ' + err);
+        }
+    }
+
+    async getAllComparisonsDate() {
+        try {
+            let locator = XPATH.comparisonsContainer + "/div[contains(@id,'TextAndDateBlock')]" + "/span[@class='date']";
+            await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+            return await this.getText(locator);
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_dlg_date');
+            throw new Error("PublishReport modal dialog, date in Comparisons Block, screenshot:  " + screenshot + ' ' + err);
+        }
+    }
+
+    async waitForShowEntireContentCheckboxNotDisplayed() {
+        let checkBoxInput = this.showEntireContentCheckbox + lib.CHECKBOX_INPUT;
+        return await this.waitForElementNotDisplayed(checkBoxInput, appConst.mediumTimeout);
+    }
+
+    async clickOnShowEntireContentCheckbox(index) {
+        let checkBoxInput = this.showEntireContentCheckbox + "/label";
         await this.waitForElementDisplayed(this.showEntireContentCheckbox, appConst.mediumTimeout);
         let res = await this.findElements(checkBoxInput);
-        if(index > res.length){
+        if (index > res.length) {
+            throw new Error('Publish report modal dialog, the number of checkboxes less then expected');
+        }
+        return await res[index].click();
+    }
+
+    async isShowEntireContentCheckboxSelected(index) {
+        let checkBoxInput = this.showEntireContentCheckbox + lib.CHECKBOX_INPUT;
+        await this.waitForElementDisplayed(this.showEntireContentCheckbox, appConst.mediumTimeout);
+        let res = await this.findElements(checkBoxInput);
+        if (index > res.length) {
             throw new Error('Publish report modal dialog, the number of checkboxes less then expected');
         }
         return await res[index].isSelected();
@@ -41,12 +117,22 @@ class PublishReportDialog extends Page {
         return await this.pause(200);
     }
 
-    async waitForDialogOpened() {
+    async waitForDialogLoaded() {
         try {
             return await this.waitForElementDisplayed(this.printButton, appConst.mediumTimeout)
         } catch (err) {
             let screenshot = await this.saveScreenshotUniqueName('err_load_publish_report_dlg');
             throw new Error("PublishReport  dialog was not loaded! screenshot:  " + screenshot + ' ' + err);
+        }
+    }
+
+    async waitForPrintButtonEnabled() {
+        try {
+            await this.waitForElementDisplayed(this.printButton, appConst.mediumTimeout);
+            await this.waitForElementEnabled(this.printButton, appConst.mediumTimeout);
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_publish_report_print_btn');
+            throw new Error("PublishReport modal dialog - Print button, screenshot:  " + screenshot + ' ' + err);
         }
     }
 
@@ -57,7 +143,6 @@ class PublishReportDialog extends Page {
     async waitForDialogClosed() {
         await this.waitForElementNotDisplayed(XPATH.container, appConst.shortTimeout);
     }
-
 }
 
 module.exports = PublishReportDialog;
---
+++ testing/specs/publish.report.dialog.spec.js
@@ -0,0 +1,135 @@
+/**
+ * Created on 27.11.2023
+ */
+const chai = require('chai');
+const assert = chai.assert;
+const webDriverHelper = require('../libs/WebDriverHelper');
+const appConst = require('../libs/app_const');
+const PublishReportDialog = require('../page_objects/publish.report.dialog');
+const studioUtils = require('../libs/studio.utils.js');
+const contentBuilder = require("../libs/content.builder");
+const ContentWizardPanel = require('../page_objects/wizardpanel/content.wizard.panel');
+const WizardPublishReportWidget = require('../page_objects/wizardpanel/details/wizard.publish.report.widget');
+const ContentPublishDialog = require('../page_objects/content.publish.dialog');
+const ContentUnpublishDialog = require('../page_objects/content.unpublish.dialog');
+
+describe('publish.report.dialog.spec: tests for publish report dialog', function () {
+    this.timeout(appConst.SUITE_TIMEOUT);
+    // set up the standalone mode if WDIO-mode is not used:
+    if (typeof browser === 'undefined') {
+        webDriverHelper.setupBrowser();
+    }
+
+    let FOLDER1;
+    const ITEM_OFFLINE_TEXT = 'Item went offline';
+    const ITEM_ONLINE_TEXT = 'Item went online';
+    const ITEM_REPUBLISHED_HEADER = 'Comparing';
+    const ITEM_REPUBLISHED_SUBTITLE = 'NB: Item was offline from';
+    const CURRENT_DATE = new Date().toJSON().slice(0, 10);
+
+    it(`Precondition: new folder should be added`,
+        async () => {
+            let displayName1 = appConst.generateRandomName('folder');
+            FOLDER1 = contentBuilder.buildFolder(displayName1);
+            await studioUtils.doAddReadyFolder(FOLDER1);
+        });
+
+    it(`GIVEN existing folder has been published in the wizard WHEN  'Generate' button has been pressed in 'Publish report' widget THEN publish report modal dialog should appear`,
+        async () => {
+            let contentWizard = new ContentWizardPanel();
+            let wizardPublishReportWidget = new WizardPublishReportWidget();
+            let contentPublishDialog = new ContentPublishDialog();
+            let publishReportDialog = new PublishReportDialog();
+            // 1. Select and open the folder:
+            await studioUtils.selectAndOpenContentInWizard(FOLDER1.displayName);
+            await contentWizard.clickOnPublishButton();
+            await contentPublishDialog.waitForDialogOpened();
+            await contentPublishDialog.clickOnPublishNowButton();
+            await contentWizard.waitForNotificationMessage();
+            // 2. Open 'Publish Report' widget:
+            await contentWizard.openPublishReportWidget();
+            await wizardPublishReportWidget.waitForWidgetLoaded();
+            // 3. Click on 'Generate' button:
+            await wizardPublishReportWidget.clickOnGenerateButton();
+            // 4. Verify that 'Print' button is displayed:
+            await publishReportDialog.waitForDialogLoaded();
+            await publishReportDialog.waitForPrintButtonEnabled();
+            // 5. 'Show entire content' checkbox should not be displayed:
+            await publishReportDialog.waitForShowEntireContentCheckboxNotDisplayed();
+            // 6. Verify the 'Item went online' in the header of the single comparison block:
+            let actualText = await publishReportDialog.getHeaderInComparisonBlock(0);
+            assert.equal(actualText, ITEM_ONLINE_TEXT, "'Item went online' - this text should be displayed in the single comparison block");
+        });
+
+    it(`GIVEN existing folder has been unpublished in the wizard WHEN 'Publish report' modal dialog has been opened THEN 'Item went offline' text should appear`,
+        async () => {
+            let contentWizard = new ContentWizardPanel();
+            let wizardPublishReportWidget = new WizardPublishReportWidget();
+            let unpublishDialog = new ContentUnpublishDialog();
+            let publishReportDialog = new PublishReportDialog();
+            // 1. Select and open the folder:
+            await studioUtils.selectAndOpenContentInWizard(FOLDER1.displayName);
+            // 2. Click on Unpublish button:
+            await contentWizard.clickOnUnpublishButton();
+            await unpublishDialog.waitForDialogOpened();
+            await unpublishDialog.clickOnUnpublishButton();
+            await unpublishDialog.waitForDialogClosed();
+            await contentWizard.waitForNotificationMessage();
+            // 3. Open 'Publish Report' widget:
+            await contentWizard.openPublishReportWidget();
+            await wizardPublishReportWidget.waitForWidgetLoaded();
+            // 4. Click on 'Generate' button:
+            await wizardPublishReportWidget.clickOnGenerateButton();
+            await publishReportDialog.waitForDialogLoaded();
+            // 5. Verify that the Comparisons-block with 'Item went offline' text gets visible in the modal dialog:
+            let actualText = await publishReportDialog.getAllComparisonsBlockHeader();
+            assert.equal(actualText, ITEM_OFFLINE_TEXT, "'Item went offline' should be displayed in the modal dialog");
+            // 6. Verify the date in the 'Item went offline' block:
+            let actualDate = await publishReportDialog.getAllComparisonsDate();
+            assert.isTrue(actualDate.includes(CURRENT_DATE), "Current date should be displayed in the text and date block");
+            // 7. Verify  that the comparison block remains visible -  'Item went online' text should be in the header of the single comparison block
+            actualText = await publishReportDialog.getHeaderInComparisonBlock(0);
+            assert.equal(actualText, ITEM_ONLINE_TEXT, "Item went online - this text should be displayed in the comparison block");
+        });
+
+    it(`GIVEN existing folder has been published in the second time WHEN 'Publish report' modal dialog has been opened THEN 'Comparing' text should appear in the modal dialog`,
+        async () => {
+            let contentWizard = new ContentWizardPanel();
+            let wizardPublishReportWidget = new WizardPublishReportWidget();
+            let contentPublishDialog = new ContentPublishDialog();
+            let publishReportDialog = new PublishReportDialog();
+            // 1. Select and open the folder:
+            await studioUtils.selectAndOpenContentInWizard(FOLDER1.displayName);
+            // 2. Click on 'Publish' button then re-publish the content again:
+            await contentWizard.clickOnPublishButton();
+            await contentPublishDialog.waitForDialogOpened();
+            await contentPublishDialog.clickOnPublishNowButton();
+            await contentWizard.waitForNotificationMessage();
+            // 3. Open 'Publish Report' widget:
+            await contentWizard.openPublishReportWidget();
+            await wizardPublishReportWidget.waitForWidgetLoaded();
+            // 4. Open 'Publish Report' modal dialog:
+            await wizardPublishReportWidget.clickOnGenerateButton();
+            await publishReportDialog.waitForDialogLoaded();
+            // 5. Verify 'Comparing' text in the header of the single comparison block:
+            let actualText = await publishReportDialog.getHeaderInComparisonBlock(0);
+            assert.equal(actualText, ITEM_REPUBLISHED_HEADER, "'Comparing' - this text should be displayed in the single comparison block");
+            let actualDate = await publishReportDialog.getDateInHeaderOfComparisonBlock(0);
+            assert.isTrue(actualDate.includes(CURRENT_DATE), "Current date should be displayed in the text and date block");
+            // 6. Verify the text in the subtitle of the single comparison block - 'NB: Item was offline from'
+            let subTitle = await publishReportDialog.getSubtitleInComparisonBlock(0);
+            assert.equal(subTitle, ITEM_REPUBLISHED_SUBTITLE,
+                "'NB: Item was offline from' - should be displayed in the subtitle of the comparison block");
+            // 7. Verify that 'Show entire content' checkbox is displayed and not selected:
+            let isSelected = await publishReportDialog.isShowEntireContentCheckboxSelected(0);
+            assert.isFalse(isSelected, "'Show entire content' checkbox should not be selected");
+        });
+
+    beforeEach(async () => {
+        return await studioUtils.navigateToContentStudioCloseProjectSelectionDialog();
+    });
+    afterEach(() => studioUtils.doCloseAllWindowTabsAndSwitchToHome());
+    before(() => {
+        return console.log('specification is starting: ' + this.title);
+    });
+});
