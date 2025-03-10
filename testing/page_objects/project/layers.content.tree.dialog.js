1a7dea507daf7b79fd3b120ad47bb5447e5f1efe
sgauruseu <sig@eninic.com>
2025-03-10 14:36:53 +0300

    ui-tests to verify bug in layers modal dialog #1516

--- testing/page_objects/project/layers.content.tree.dialog.js
+++ testing/page_objects/project/layers.content.tree.dialog.js
@@ -6,13 +6,16 @@ const lib = require('../../libs/elements');
 const appConst = require('../../libs/app_const');
 
 const XPATH = {
-    container: "//div[contains(@id,'LayersContentTreeDialog')]",
-    title: "//h2[@class='title']",
-    layersTreeList: "//ul[contains(@id,'LayersContentTreeList')]",
-    itemView: "//div[contains(@id,'LayerContentViewDataBlock')]",
+    container: `//div[contains(@id,'LayersContentTreeDialog')]`,
+    title: `//h2[@class='title']`,
+    layersTreeList: `//ul[contains(@id,'LayersContentTreeList')]`,
+    itemView: `//div[contains(@id,'LayerContentViewDataBlock')]`,
     layerByName: name => {
         return `//div[@class='layer-name' and contains(.,'${name}')]`
     },
+    layerDataBlockByName: layerName => {
+        return `//div[contains(@id,'LayerContentViewDataBlock') and descendant::div[contains(@class,'layer-name') and contains(.,'${layerName}')]]`;
+    }
 };
 
 class LayersContentTreeDialog extends Page {
@@ -26,17 +29,19 @@ class LayersContentTreeDialog extends Page {
             await this.clickOnElement(this.cancelButtonTop);
             return await this.waitForDialogClosed();
         } catch (err) {
-            this.saveScreenshot('err_layers_tree_click_on_cancel_button');
-            throw new Error('Layers Content Tree dialog, error when clicking on Cancel(Top) button  ' + err);
+            let screenshot = await this.saveScreenshotUniqueName('err_layers_tree_click_on_cancel_button');
+            throw new Error(`Layers Content Tree dialog, error when clicking on Cancel(Top) button, screenshot:${screenshot}  ` + err);
         }
     }
 
-    waitForDialogLoaded() {
-        let selector = XPATH.container + XPATH.layersTreeList;
-        return this.waitForElementDisplayed(selector, appConst.shortTimeout).catch(err => {
-            this.saveScreenshot('err_open_layers_tree_dialog');
-            throw new Error('Layers Content Tree dialog dialog should be opened!' + err);
-        });
+    async waitForDialogLoaded() {
+        try {
+            let selector = XPATH.container + XPATH.layersTreeList;
+            await this.waitForElementDisplayed(selector, appConst.shortTimeout)
+        } catch (err) {
+            let screenshot = await this.saveScreenshotUniqueName('err_open_layers_tree_dialog');
+            throw new Error(`Layers Content Tree dialog dialog should be opened! screenshot:${screenshot}` + err);
+        }
     }
 
     isDialogLoaded() {
@@ -47,7 +52,8 @@ class LayersContentTreeDialog extends Page {
         try {
             return await this.waitForElementNotDisplayed(XPATH.container, appConst.shortTimeout);
         } catch (err) {
-            throw new Error("Layers Content Tree dialog should be closed " + err);
+            let screenshot = await this.saveScreenshotUniqueName('err_layers_tree_dialog_closed');
+            throw new Error(`Layers Content Tree dialog should be closed ${screenshot} ` + err);
         }
     }
 
@@ -56,7 +62,7 @@ class LayersContentTreeDialog extends Page {
     }
 
     async getLayersName() {
-        let locator = XPATH.container + XPATH.itemView + "//div[@class='layer-name']";
+        let locator = XPATH.container + XPATH.itemView + `//div[@class='layer-name']`;
         await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
         return await this.getTextInElements(locator);
 
@@ -66,7 +72,7 @@ class LayersContentTreeDialog extends Page {
         try {
             return await this.waitForElementDisplayed(this.cancelButtonTop, appConst.shortTimeout);
         } catch (err) {
-            throw new Error("Layers Content Tree dialog dialog - Cancel button is not displayed :" + err);
+            throw new Error('Layers Content Tree dialog dialog - Cancel button is not displayed :' + err);
         }
     }
 
@@ -77,12 +83,18 @@ class LayersContentTreeDialog extends Page {
     }
 
     async getButtonLabelInItemView(layerName) {
-        let itemLocator = XPATH.container + XPATH.layersTreeList +
-                          `//div[contains(@id,'LayerContentViewDataBlock') and descendant::div[contains(@class,'layer-name') and contains(.,'${layerName}')]]`;
-        let buttonLocator = itemLocator + "//button[contains(@id,'LayersContentActionButton')]";
+        let itemLocator = XPATH.container + XPATH.layersTreeList + XPATH.layerDataBlockByName(layerName);
+        let buttonLocator = itemLocator + `//button[contains(@id,'LayersContentActionButton')]`;
         await this.waitForElementDisplayed(buttonLocator, appConst.mediumTimeout);
-        return await this.getText(buttonLocator + "//span");
+        return await this.getText(buttonLocator + '//span');
+    }
+
+    async getContentStatus(layerName) {
+        let locator = XPATH.container + XPATH.layersTreeList + XPATH.layerDataBlockByName(layerName) + `//div[contains(@id,'LayerContentViewHeader')]//div[contains(@class,'status')]`;
+        await this.waitForElementDisplayed(locator, appConst.mediumTimeout);
+        return await this.getTextInElements(locator);
     }
 }
+
 module.exports = LayersContentTreeDialog;
 
