9eb79f75331d8816855e6d494c99ab93cad722b3
Aliaksandr Shklianko <asl@enonic.com>
2025-06-24 12:03:42 +0200

    Details widget: add new fields #1597

--- src/main/resources/assets/js/widgets/ArchivePropertiesWidgetItemView.ts
+++ src/main/resources/assets/js/widgets/ArchivePropertiesWidgetItemView.ts
@@ -1,32 +1,71 @@
-import {i18n} from '@enonic/lib-admin-ui/util/Messages';
-import {Element} from '@enonic/lib-admin-ui/dom/Element';
+import {Path} from '@enonic/lib-admin-ui/rest/Path';
 import {DateTimeFormatter} from '@enonic/lib-admin-ui/ui/treegrid/DateTimeFormatter';
+import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {BasePropertiesWidgetItemView} from 'lib-contentstudio/app/view/context/widget/details/BasePropertiesWidgetItemView';
-
+import {BasePropertiesWidgetItemViewHelper} from 'lib-contentstudio/app/view/context/widget/details/BasePropertiesWidgetItemViewHelper';
+import {PropertiesWidgetItemViewHelper} from 'lib-contentstudio/app/view/context/widget/details/PropertiesWidgetItemViewHelper';
+import {PropertiesWidgetItemViewValue} from 'lib-contentstudio/app/view/context/widget/details/PropertiesWidgetItemViewValue';
 
 export class ArchivePropertiesWidgetItemView
     extends BasePropertiesWidgetItemView {
 
-    protected layoutProperties(): void {
-        super.layoutProperties();
-
-        const archived: string = i18n('status.archived');
-        const archivedDate: string = DateTimeFormatter.createHtml(this.item.getContentSummary()?.getArchivedTime());
-        this.insertKeyValue(archived, archivedDate, this.getIndexToInsertArchived());
-    }
-
-    private getIndexToInsertArchived(): number {
-        const indexOfModified: number =
-            this.list.getChildren().findIndex((child: Element) => child.getHtml().indexOf(i18n('field.modified')) > -1);
-
-        if (indexOfModified > -1) {
-            return indexOfModified + 2;
-        }
-
-        return this.list.getChildren().length;
+    protected createHelper(): PropertiesWidgetItemViewHelper {
+        return new ArchivePropertiesWidgetItemViewHelper();
     }
 
     protected layoutEditLink(): void {
         //
+    }
+}
+
+export class ArchivePropertiesWidgetItemViewHelper
+    extends BasePropertiesWidgetItemViewHelper {
+
+    protected getPrincipalKeysToFetch(): Set<string> {
+        const principals =  super.getPrincipalKeysToFetch();
+
+        principals.add(this.item.getContentSummary().getArchivedBy().toString());
+
+        return principals;
+    }
+
+    protected doGenerateProps(): Map<string, PropertiesWidgetItemViewValue> {
+        const propsMap: Map<string, PropertiesWidgetItemViewValue> = new Map<string, PropertiesWidgetItemViewValue>();
+
+        this.setPropsFieldId(propsMap);
+        this.setPropsType(propsMap);
+        this.setPropsApp(propsMap);
+        this.setPropsCreated(propsMap);
+        this.setPropsModified(propsMap);
+        this.setPropsArchived(propsMap);
+        this.setPropsOriginalPath(propsMap);
+        this.setPropsPublishFirstTime(propsMap);
+        this.setPropsLang(propsMap);
+        this.setPropsOwner(propsMap);
+
+        return propsMap;
+    }
+
+    private setPropsArchived(propsMap: Map<string, PropertiesWidgetItemViewValue>): void {
+        const archivedBy = this.item.getContentSummary().getArchivedBy()?.toString();
+        const archivedTime = this.item.getContentSummary().getArchivedTime();
+
+        if (archivedTime && archivedBy) {
+            const displayName = this.getFetchedPrincipal(archivedBy)?.getDisplayName();
+            const archivedByText = i18n('text.by', DateTimeFormatter.createHtml(archivedTime), displayName || archivedBy);
+
+            propsMap.set(i18n('field.archived'), new PropertiesWidgetItemViewValue(archivedByText));
+        }
+    }
+
+    private setPropsOriginalPath(propsMap: Map<string, PropertiesWidgetItemViewValue>): void {
+        const originalParentPath = this.item.getContentSummary().getOriginalParentPath();
+
+        if (originalParentPath) {
+            const separator = originalParentPath === Path.DEFAULT_ELEMENT_DIVIDER ? '' : Path.DEFAULT_ELEMENT_DIVIDER;
+            const path = `${originalParentPath}${separator}${this.item.getContentSummary().getOriginalName()}`;
+
+            propsMap.set(i18n('field.originalPath'), new PropertiesWidgetItemViewValue(path, path));
+        }
     }
 }
