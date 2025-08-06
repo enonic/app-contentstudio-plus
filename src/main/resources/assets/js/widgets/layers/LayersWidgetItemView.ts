e5d43d5052a40cbc0f64c21572928d304351877b
Aliaksandr Shklianko <asl@enonic.com>
2025-08-06 13:07:55 +0200

    Layers Widget should show descendants in the context panel #1409

--- src/main/resources/assets/js/widgets/layers/LayersWidgetItemView.ts
+++ src/main/resources/assets/js/widgets/layers/LayersWidgetItemView.ts
@@ -1,40 +1,36 @@
+import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {LayersView} from './LayersView';
 import Q from 'q';
 import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
 import {ShowAllContentLayersButton} from './ShowAllContentLayersButton';
 import {MultiLayersContentLoader} from './MultiLayersContentLoader';
 import {LayerContent} from './LayerContent';
-import {DefaultErrorHandler} from '@enonic/lib-admin-ui/DefaultErrorHandler';
 import {DivEl} from '@enonic/lib-admin-ui/dom/DivEl';
-import {LayersContentTreeDialog} from './dialog/LayersContentTreeDialog';
-import {ProjectCreatedEvent} from 'lib-contentstudio/app/settings/event/ProjectCreatedEvent';
-import {ProjectUpdatedEvent} from 'lib-contentstudio/app/settings/event/ProjectUpdatedEvent';
-import {ProjectDeletedEvent} from 'lib-contentstudio/app/settings/event/ProjectDeletedEvent';
 import {Store} from '@enonic/lib-admin-ui/store/Store';
+import {ContentSummaryAndCompareStatus} from 'lib-contentstudio/app/content/ContentSummaryAndCompareStatus';
 import {ContentServerEventsHandler} from 'lib-contentstudio/app/event/ContentServerEventsHandler';
-import {ContentServerChangeItem} from 'lib-contentstudio/app/event/ContentServerChangeItem';
+import {ProjectCreatedEvent} from 'lib-contentstudio/app/settings/event/ProjectCreatedEvent';
+import {ProjectDeletedEvent} from 'lib-contentstudio/app/settings/event/ProjectDeletedEvent';
+import {ProjectUpdatedEvent} from 'lib-contentstudio/app/settings/event/ProjectUpdatedEvent';
+import * as Q from 'q';
+import {LayerContent} from './LayerContent';
+import {LayersContentTreeList} from './LayersContentTreeList';
+import {MultiLayersContentLoader} from './MultiLayersContentLoader';
 
 export class LayersWidgetItemView
     extends DivEl {
 
-    private readonly layersView: LayersView;
-
-    private readonly showAllButton: ShowAllContentLayersButton;
+    private readonly layersContentTreeList: LayersContentTreeList;
 
     private readonly loader: MultiLayersContentLoader;
-
-    private layerContentItems: LayerContent[] = [];
 
     private item: ContentSummaryAndCompareStatus;
 
     private constructor() {
         super('layers-widget-item-view');
 
-        this.layersView = new LayersView();
+        this.layersContentTreeList = new LayersContentTreeList();
         this.loader = new MultiLayersContentLoader();
-
-        this.showAllButton = new ShowAllContentLayersButton();
-        this.showAllButton.hide();
 
         this.initListeners();
     }
@@ -52,8 +48,6 @@ export class LayersWidgetItemView
 
     setContentAndUpdateView(item: ContentSummaryAndCompareStatus): Q.Promise<void> {
         this.item = item;
-
-        this.showAllButton.hide();
         this.loader.setItem(item);
 
         return this.reload();
@@ -61,13 +55,7 @@ export class LayersWidgetItemView
 
     reload(): Q.Promise<void> {
         return this.loader.load().then((items: LayerContent[]) => {
-            this.layerContentItems = items;
-            this.layersView.setItems(items);
-            this.showAllButton.updateLabelAndVisibility(items);
-
-            if (LayersContentTreeDialog.get().isOpen()) {
-                LayersContentTreeDialog.get().setItems(items);
-            }
+            this.layersContentTreeList.setItems(items);
 
             return Q();
         });
@@ -75,17 +63,12 @@ export class LayersWidgetItemView
 
     doRender(): Q.Promise<boolean> {
         return super.doRender().then((rendered: boolean) => {
-            this.appendChild(this.layersView);
-            this.appendChild(this.showAllButton);
+            this.appendChild(this.layersContentTreeList);
             return rendered;
         });
     }
 
     private initListeners(): void {
-        this.showAllButton.getAction().onExecuted(() => {
-            LayersContentTreeDialog.get().setItems(this.layerContentItems).open();
-        });
-
         this.initProjectEventListeners();
         this.initContentEventListeners();
     }
@@ -105,20 +88,6 @@ export class LayersWidgetItemView
     private initContentEventListeners(): void {
         const serverEventsHandler: ContentServerEventsHandler = ContentServerEventsHandler.getInstance();
 
-        const contentDeletedHandler: (items: ContentServerChangeItem[]) => void = (items: ContentServerChangeItem[]) => {
-            if (!this.isVisible()) {
-                return;
-            }
-
-            const id: string = this.item.getContentId().toString();
-
-            if (items.some((deletedOrArchivedItem: ContentServerChangeItem) => deletedOrArchivedItem.getContentId().toString() === id)) {
-                if (LayersContentTreeDialog.get().isOpen()) {
-                    LayersContentTreeDialog.get().close();
-                }
-            }
-        };
-
         const updateHandler: (items: ContentSummaryAndCompareStatus[]) => void = (items: ContentSummaryAndCompareStatus[]) => {
             if (!this.isVisible()) {
                 return;
@@ -131,7 +100,6 @@ export class LayersWidgetItemView
             }
         };
 
-        serverEventsHandler.onContentDeleted(contentDeletedHandler);
         serverEventsHandler.onContentUpdated(updateHandler);
     }
 }
