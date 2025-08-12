7fb76f1a4703e2f96936d414590839de7ce2d4b3
Mikita Taukachou <edloidas@gmail.com>
2025-08-12 14:42:17 +0300

    Migrate to newer version of JS #1635

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const ErrorLoggerPlugin = require('error-logger-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ProvidePlugin = require('webpack/lib/ProvidePlugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -12,78 +11,84 @@ const assets = path.join(__dirname, '/build/resources/main/assets');
 const swcConfig = JSON.parse(fs.readFileSync('./.swcrc'));
 
 module.exports = {
-    context: path.join(__dirname, '/src/main/resources/assets'),
-    entry: {
-        'js/archive': './js/archive.ts',
-        'styles/main': './styles/main.less',
-        'js/widgets/layers': './js/widgets/layers/main.ts',
-        'js/widgets/variants': './js/widgets/variants/main.ts',
-        'js/widgets/publish-report': './js/widgets/publish-report/main.ts',
-        'styles/widgets/layers': './styles/widgets/layers/main.less',
-        'styles/widgets/variants': './styles/widgets/variants/main.less',
-        'styles/widgets/publish-report': './styles/widgets/publish-report/main.less',
-        'styles/license': './styles/license.less'
-    },
-    output: {
-        path: assets,
-        filename: './[name].js'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.less', '.css']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'swc-loader',
-                        options: {
-                            ...swcConfig,
-                            sourceMaps: isProd ? false : 'inline',
-                            inlineSourcesContent: !isProd,
-                        },
-                    },
-                ],
+  context: path.join(__dirname, '/src/main/resources/assets'),
+  entry: {
+    'js/archive': './js/archive.ts',
+    'styles/main': './styles/main.less',
+    'js/widgets/layers': './js/widgets/layers/main.ts',
+    'js/widgets/variants': './js/widgets/variants/main.ts',
+    'js/widgets/publish-report': './js/widgets/publish-report/main.ts',
+    'styles/widgets/layers': './styles/widgets/layers/main.less',
+    'styles/widgets/variants': './styles/widgets/variants/main.less',
+    'styles/widgets/publish-report':
+      './styles/widgets/publish-report/main.less',
+    'styles/license': './styles/license.less',
+  },
+  output: {
+    path: assets,
+    filename: './[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.less', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'swc-loader',
+            options: {
+              ...swcConfig,
+              sourceMaps: isProd ? false : 'inline',
+              inlineSourcesContent: !isProd,
             },
-            {
-                test: /\.less$/,
-                use: [
-                    {loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}},
-                    {loader: 'css-loader', options: {sourceMap: !isProd, importLoaders: 1}},
-                    {loader: 'postcss-loader', options: {sourceMap: !isProd}},
-                    {loader: 'less-loader', options: {sourceMap: !isProd}},
-                ]
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    keep_classnames: true,
-                    keep_fnames: true
-                }
-            })
-        ]
-    },
-    plugins: [
-        new ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: './styles/[id].css'
-        }),
-        new CircularDependencyPlugin({
-            exclude: /a\.js|node_modules/,
-            failOnError: true
-        }),
-        //new ErrorLoggerPlugin({showColumn: false}),
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '../' },
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: !isProd, importLoaders: 1 },
+          },
+          { loader: 'postcss-loader', options: { sourceMap: !isProd } },
+          { loader: 'less-loader', options: { sourceMap: !isProd } },
+        ],
+      },
     ],
-    mode: isProd ? 'production' : 'development',
-    devtool: isProd ? false : 'source-map',
-    performance: { hints: false }
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: './styles/[id].css',
+    }),
+    new CircularDependencyPlugin({
+      exclude: /a\.js|node_modules/,
+      failOnError: true,
+    }),
+  ],
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? false : 'source-map',
+  performance: { hints: false },
 };
