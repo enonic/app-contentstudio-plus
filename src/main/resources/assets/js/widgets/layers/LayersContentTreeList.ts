eeedffe508cf107f655180db535f53e794b9e6da
Alex Shklianko <asl@enonic.com>
2025-08-19 15:47:44 +0200

    Layers Widget should show descendants in the context panel #1409

--- src/main/resources/assets/js/widgets/layers/LayersContentTreeList.ts
+++ src/main/resources/assets/js/widgets/layers/LayersContentTreeList.ts
@@ -1,24 +1,28 @@
 import {ListBox} from '@enonic/lib-admin-ui/ui/selector/list/ListBox';
 import {LayerContent} from './LayerContent';
-import {LayerContentViewDataBlock} from './LayerContentViewDataBlock';
 import {LayerContentView} from './LayerContentView';
 import {LayersContentTreeListHelper} from './LayersContentTreeListHelper';
-
 
 export class LayersContentTreeList
     extends ListBox<LayerContent> {
 
+    public static readonly HIDE_OTHER_TREES_CLASS: string = 'hide-other-trees';
+
     private helper: LayersContentTreeListHelper;
 
-    private activeItemView: LayerContentViewDataBlock;
+    private activeItemView: LayerContentView;
+
+    private hasLayersFromOtherTrees: boolean = false;
 
     constructor() {
-        super('layers-content-tree-list');
+        super(`layers-content-tree-list ${LayersContentTreeList.HIDE_OTHER_TREES_CLASS}`);
 
         this.helper = new LayersContentTreeListHelper();
     }
 
     setItems(items: LayerContent[], silent?: boolean): void {
+        this.hasLayersFromOtherTrees = false;
+        this.addClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS);
         this.helper.setItems(items);
         super.setItems(this.helper.toFlatTree(), silent);
     }
@@ -29,13 +33,16 @@ export class LayersContentTreeList
         return `${projectName}:${contentId}`;
     }
 
-    protected createItemView(item: LayerContent, readOnly: boolean): LayerContentViewDataBlock {
-        const itemView: LayerContentViewDataBlock = new LayerContentViewDataBlock(item, `${LayerContentView.VIEW_CLASS}-data`);
-        itemView.addClass(`level-${this.helper.calcLevel(item)}`);
+    protected createItemView(item: LayerContent, readOnly: boolean): LayerContentView {
+        const itemView: LayerContentView = new LayerContentView(item, this.helper.calcLevel(item));
+        itemView.addClass(`layers-content-tree-list-item`);
+        const isFromCurrentProjectTree = this.helper.isFromCurrentProjectTree(item);
+        itemView.toggleClass('non-current-tree', !isFromCurrentProjectTree);
+        this.hasLayersFromOtherTrees = !isFromCurrentProjectTree || this.hasLayersFromOtherTrees;
 
         itemView.onClicked(() => {
             if (this.activeItemView) {
-                this.activeItemView.removeClass('expanded');
+                this.activeItemView.getDataBlock().removeClass('expanded');
             }
 
             if (this.activeItemView === itemView) {
@@ -44,10 +51,22 @@ export class LayersContentTreeList
             }
 
             this.activeItemView = itemView;
-            this.activeItemView.addClass('expanded');
+            this.activeItemView.getDataBlock().addClass('expanded');
         });
+
+        if (this.helper.isCurrentProject(item)) {
+            this.activeItemView = itemView;
+            this.activeItemView.getDataBlock().addClass('expanded');
+        }
 
         return itemView;
     }
 
+    hasLayersToHide(): boolean {
+        return this.hasLayersFromOtherTrees;
+    }
+
+    toggleHiddenLayersVisible(): void {
+        this.toggleClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS, !this.hasClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS));
+    }
 }
