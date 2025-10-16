e44cc9c40092a361144eed9f8bae33f4cd88a89e
alansemenov <ase@enonic.com>
2025-10-16 14:15:04 +0200

    Layers widget should show entire tree in modal dialog #1679

--- src/main/resources/assets/js/widgets/layers/LayersContentTreeList.ts
+++ src/main/resources/assets/js/widgets/layers/LayersContentTreeList.ts
@@ -1,3 +1,4 @@
+import {UlEl} from '@enonic/lib-admin-ui/dom/UlEl';
 import {ListBox} from '@enonic/lib-admin-ui/ui/selector/list/ListBox';
 import {LayerContent} from './LayerContent';
 import {LayerContentView} from './LayerContentView';
@@ -6,25 +7,43 @@ import {LayersContentTreeListHelper} from './LayersContentTreeListHelper';
 export class LayersContentTreeList
     extends ListBox<LayerContent> {
 
-    public static readonly HIDE_OTHER_TREES_CLASS: string = 'hide-other-trees';
-
     private helper: LayersContentTreeListHelper;
 
     private activeItemView: LayerContentView;
 
-    private hasLayersFromOtherTrees: boolean = false;
+    private allItems: LayerContent[] = [];
+
+    private previousLevel: number = 0;
+
+    private wrappers: Map<number, UlEl> = new Map<number, UlEl>();
+
+    private previousItemView: LayerContentView;
 
     constructor() {
-        super(`layers-content-tree-list ${LayersContentTreeList.HIDE_OTHER_TREES_CLASS}`);
+        super(`layers-content-tree-list`);
 
         this.helper = new LayersContentTreeListHelper();
     }
 
     setItems(items: LayerContent[], silent?: boolean): void {
-        this.hasLayersFromOtherTrees = false;
-        this.addClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS);
+        this.resetWrappers();
         this.helper.setItems(items);
         super.setItems(this.helper.toFlatTree(), silent);
+    }
+
+    setAllItems(items: LayerContent[], silent?: boolean): void {
+        this.allItems = items;
+
+        this.helper.setItems(this.allItems);
+
+        const itemsFromCurrentProjectTree: LayerContent[] =
+            items.filter((item: LayerContent) => this.helper.isFromCurrentProjectTree(item));
+
+        this.setItems(itemsFromCurrentProjectTree);
+    }
+
+    getAllItems(): LayerContent[] {
+        return this.allItems;
     }
 
     protected getItemId(item: LayerContent): string {
@@ -33,40 +52,63 @@ export class LayersContentTreeList
         return `${projectName}:${contentId}`;
     }
 
+    protected addItemView(item: LayerContent, readOnly: boolean = false): LayerContentView {
+        const itemView: LayerContentView = this.createItemView(item, readOnly);
+        this.itemViews.set(this.getItemId(item), itemView);
+        const currentLevel = itemView.getLevel();
+        const wrapper = currentLevel > this.previousLevel ?
+                        new UlEl('layers-item-view-wrapper') : this.wrappers.get(currentLevel);
+
+        if (currentLevel > this.previousLevel) {
+            this.previousItemView.setChildBlock(wrapper);
+            this.wrappers.set(currentLevel, wrapper);
+        }
+
+        wrapper.appendChild(itemView);
+
+        this.previousLevel = currentLevel;
+        this.previousItemView = itemView;
+
+        return itemView;
+    }
+
     protected createItemView(item: LayerContent, readOnly: boolean): LayerContentView {
         const itemView: LayerContentView = new LayerContentView(item, this.helper.calcLevel(item));
         itemView.addClass(`layers-content-tree-list-item`);
-        const isFromCurrentProjectTree = this.helper.isFromCurrentProjectTree(item);
-        itemView.toggleClass('non-current-tree', !isFromCurrentProjectTree);
-        this.hasLayersFromOtherTrees = !isFromCurrentProjectTree || this.hasLayersFromOtherTrees;
 
-        itemView.onClicked(() => {
+        itemView.onClicked((e: MouseEvent) => {
             if (this.activeItemView) {
-                this.activeItemView.getDataBlock().removeClass('expanded');
+                this.activeItemView.removeClass('expanded');
             }
 
             if (this.activeItemView === itemView) {
                 this.activeItemView = null;
+                e.stopPropagation();
                 return;
             }
 
             this.activeItemView = itemView;
-            this.activeItemView.getDataBlock().addClass('expanded');
+            this.activeItemView.addClass('expanded');
+
+            e.stopPropagation();
         });
 
         if (this.helper.isCurrentProject(item)) {
             this.activeItemView = itemView;
-            this.activeItemView.getDataBlock().addClass('expanded');
+            this.activeItemView.addClass('expanded');
         }
 
         return itemView;
     }
 
     hasLayersToHide(): boolean {
-        return this.hasLayersFromOtherTrees;
+        return this.getAllItems().length > this.getItems().length;
     }
 
-    toggleHiddenLayersVisible(): void {
-        this.toggleClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS, !this.hasClass(LayersContentTreeList.HIDE_OTHER_TREES_CLASS));
+    private resetWrappers(): void {
+        this.wrappers.clear();
+        this.wrappers.set(0, this);
+        this.previousLevel = 0;
+        this.previousItemView = null;
     }
 }
